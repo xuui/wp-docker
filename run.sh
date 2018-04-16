66c9d8430d8c8509783704d1ee3fdeacd692b0e9
Xuhel <xu.hel@outlook.com>
2018-04-16 18:33:26 +0800

    add http2.0 ssl

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ docker pull nginx:1.13.12
 docker pull nginx
 
 #docker container run -d -p 8080:80 --rm --name mynginx nginx:1.13.12
-docker container run --rm --name mynginx --volume "$PWD/htmls":/usr/share/nginx/html --volume "$PWD/etc/nginx":/etc/nginx -p 8080:80 -d nginx
+#docker container run --rm --name mynginx --volume "$PWD/htmls":/usr/share/nginx/html --volume "$PWD/etc/nginx":/etc/nginx -p 8080:80 -d nginx
+docker container run --rm --name mynginx --volume "$PWD/htmls":/usr/share/nginx/html --volume "$PWD/etc/nginx":/etc/nginx -p 8080:80 -p 8081:443 -d nginx
