893038b21e97b73ada9da19bb1e66153f3e6d5b8
Xuhel <xu.hel@outlook.com>
2018-04-16 18:22:34 +0800

    nginx config.

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ docker pull nginx:alpine
 docker pull nginx:1.13.12
 docker pull nginx
 
-docker container run -d -p 8080:80 --rm --name mynginx nginx:1.13.12
-
+#docker container run -d -p 8080:80 --rm --name mynginx nginx:1.13.12
+docker container run --rm --name mynginx --volume "$PWD/htmls":/usr/share/nginx/html --volume "$PWD/conf/nginx":/etc/nginx -p 8080:80 -d nginx
