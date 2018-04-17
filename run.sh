779e471bf579c910e84d8c87af7fdad5d9fec332
Xuhel <xu.hel@outlook.com>
2018-04-17 13:05:06 +0800

    nginx for docker-compose.yml

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-docker pull nginx:alpine
-docker pull nginx:1.13.12
-docker pull nginx
+#docker pull nginx:alpine
+#docker pull nginx:1.13.12
+#docker pull nginx
 
 #docker container run -d -p 8080:80 --rm --name mynginx nginx:1.13.12
-#docker container run --rm --name mynginx --volume "$PWD/htmls":/usr/share/nginx/html --volume "$PWD/etc/nginx":/etc/nginx -p 8080:80 -d nginx
-docker container run --rm --name mynginx --volume "$PWD/htmls":/var/www/html --volume "$PWD/etc/nginx":/etc/nginx -p 8080:80 -p 8081:443 -d nginx
+#docker container run --rm --name mynginx --volume "$PWD/htmls":/usr/share/nginx/html --volume "$PWD/etc/nginx":/etc/nginx -p 8080:80 -d nginx:1.13.12
+docker container run --rm --name mynginx --volume "$PWD/htmls":/var/www/html --volume "$PWD/etc/nginx":/etc/nginx -p 8080:80 -p 8081:443 -d nginx:1.13.12
+
+
