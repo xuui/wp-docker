6c513bba08417234442328a8d4bfc7b5d7609053
Xuhel <xu.hel@outlook.com>
2018-05-04 10:51:37 +0800

    php:7.2-fpm & wordpress:php7.2-fpm

--- run.sh
+++ run.sh
@@ -10,5 +10,5 @@ docker container run --rm --name myphp -d -v "$PWD/htmls":/var/www/html -p 9000:
 
 #docker container run -d -p 8080:80 --rm --name mynginx nginx:1.13.12
 #docker container run --rm --name mynginx --volume "$PWD/htmls":/usr/share/nginx/html --volume "$PWD/etc/nginx":/etc/nginx -p 80:80 -d nginx:1.13.12
-docker container run --rm --name mynginx --volume "$PWD/htmls":/var/www/html --volume "$PWD/etc/nginx":/etc/nginx -p 80:80 -p 443:443 -d --link myphp:myphp nginx:1.13.12
+docker container run --rm --name mynginx --volume "$PWD/htmls":/var/www/html --volume "$PWD/etc/nginx":/etc/nginx -p 80:80 -p 443:443 -d --link myphp:php nginx:1.13.12
 
